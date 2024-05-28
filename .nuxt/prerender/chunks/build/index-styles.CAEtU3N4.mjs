a9ade1c99a0df8dabb2e896ca5e71c3b0e99b322
Дмитрий <markelovdn@gmail.com>
2024-05-28 13:11:25 +0300

    test 3

---
+++ .nuxt/prerender/chunks/build/index-styles.CAEtU3N4.mjs
@@ -0,0 +1,6 @@
+const index_vue_vue_type_style_index_0_scoped_5e109bef_lang = ".header[data-v-5e109bef]{z-index:2}.content-below-header[data-v-5e109bef]{background-color:#fff;height:100vh;position:relative;top:-50px;z-index:1}";
+
+const indexStyles_CAEtU3N4 = [index_vue_vue_type_style_index_0_scoped_5e109bef_lang];
+
+export { indexStyles_CAEtU3N4 as default };
+//# sourceMappingURL=index-styles.CAEtU3N4.mjs.map
