7fdb292ce8d09a3c26064e6853a03467b1a9bdc8
Дмитрий <markelovdn@gmail.com>
2024-05-28 17:02:10 +0300

    test 2

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@ import { ref } from "vue";
 
 const route = useRoute();
 const rightDrawerOpen = ref(false);
+const slide = ref("style");
+const lorem =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatum obcaecati doloremque totam autem animi ipsam eveniet tempora aut.";
 
 const toggleRightDrawer = () => {
   rightDrawerOpen.value = !rightDrawerOpen.value;
@@ -13,12 +16,7 @@ const toggleRightDrawer = () => {
 <template>
   <q-header reveal class="bg-transparent text-black header">
     <q-toolbar>
-      <q-toolbar-title>
-        <q-avatar>
-          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-        </q-avatar>
-        Title
-      </q-toolbar-title>
+      <q-toolbar-title> Логотип сайта </q-toolbar-title>
 
       <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
     </q-toolbar>
@@ -40,6 +38,51 @@ const toggleRightDrawer = () => {
   </div>
   <div>
     <q-img src="img/11.jpg" />
+  </div>
+
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-carousel
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        control-color="white"
+        navigation
+        padding
+        arrows
+        height="300px"
+        class="bg-primary text-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide name="style" class="column no-wrap flex-center">
+          <q-icon name="style" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+            <q-img src="img/11.jpg" />
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="live_tv" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+            <q-img src="img/11.jpg" />
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-icon name="layers" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="map" class="column no-wrap flex-center">
+          <q-icon name="terrain" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </div>
   </div>
 </template>
 
