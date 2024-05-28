60df4f0f03af75aa19e22228653df3ddf3390cf5
Дмитрий <markelovdn@gmail.com>
2024-05-28 12:54:51 +0300

    test 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
   modules: ["nuxt-quasar-ui"],
   css: ["~/assets/css/main.css"],
+  app: {
+    baseURL: "/test_nuxt/",
+  },
+  vite: {
+    base: "/test_nuxt/",
+  },
 });
