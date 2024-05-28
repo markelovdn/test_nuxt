5371c32e6945ae8ca16b3a8e9186d73c420c2d6f
Дмитрий <markelovdn@gmail.com>
2024-05-28 12:06:48 +0300

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,4 @@
+export default defineNuxtConfig({
+  modules: ["nuxt-quasar-ui"],
+  css: ["~/assets/css/main.css"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+
+const route = useRoute();
+const rightDrawerOpen = ref(false);
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+</script>
+
+<template>
+  <q-header reveal class="bg-transparent text-black header">
+    <q-toolbar>
+      <q-toolbar-title>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+        </q-avatar>
+        Title
+      </q-toolbar-title>
+
+      <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+    </q-toolbar>
+  </q-header>
+
+  <q-drawer
+    v-model="rightDrawerOpen"
+    side="right"
+    overlay
+    behavior="mobile"
+    elevated
+  >
+    <!-- drawer content -->
+  </q-drawer>
+  <div class="content-below-header">
+    <q-img
+      src="img/tild3035-3064-4862-b438-633135376463__119785244_6641036542.jpg"
+    />
+  </div>
+  <div>
+    <q-img src="img/11.jpg" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  z-index: 2;
+}
+
+.content-below-header {
+  height: 100vh;
+  background-color: #fff;
+  position: relative;
+  top: -50px;
+  z-index: 1;
+}
+</style>
